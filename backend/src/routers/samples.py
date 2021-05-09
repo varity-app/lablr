38c51e5acc54af504b032f480e4598447c256efd
Callum Gundlach <gundlachcallum@gmail.com>
2021-05-09 10:41:30 -0400

    Implement the sample labeling endpoint

--- backend/src/routers/samples.py
+++ backend/src/routers/samples.py
@@ -5,11 +5,12 @@ Routes related to Dataset objects
 from typing import Dict, List, Optional
 import logging
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, exc
 
-from database import sample, get_db
+from database import sample, label_definition, get_db
+from util.constants import LabelVariants
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -82,7 +83,7 @@ def get_samples(
 
     # Pagination Metadata
     total = query.count()
-    query = query.offset(offset).offset(limit)
+    query = query.offset(offset).limit(limit)
 
     next_offset = offset + limit
     if next_offset >= total:
@@ -105,3 +106,72 @@ def get_samples(
         samples=query.all(),
         metadata=metadata,
     )
+
+
+class SamplePut(BaseModel):
+    """Schema of the request body for updating (labeling) a sample"""
+
+    labels: Dict[str, float]
+
+
+@router.put("/datasets/{dataset_id}/samples/{sample_id}")
+def label_sample(
+    data: SamplePut, dataset_id, sample_id, db_session: Session = Depends(get_db)
+):
+    """Label a sample"""
+
+    db_labels = (
+        db_session.query(label_definition.LabelDefinition)
+        .filter_by(dataset_id=dataset_id)
+        .all()
+    )
+
+    # Validate labels
+    for label, value in data.labels.items():
+
+        # Check that label is valid for a dataset
+        label_def = None
+        for db_label in db_labels:
+            if label == db_label.name:
+                label_def = db_label
+
+        if label_def is None:
+            raise HTTPException(
+                status_code=422,
+                detail=f"Label `{label}` is not a valid label for dataset with dataset_id `{dataset_id}`",
+            )
+
+        # Check if numerical label is within valid bounds
+        if (
+            label_def.variant == LabelVariants.NUMERICAL
+            and not label_def.minimum <= value <= label_def.maximum
+        ):
+            raise HTTPException(
+                status_code=422,
+                detail=f"Value `{value}` of label `{label}` "
+                f"must be within range ({label_def.minimum}, {label_def.maximum}).",
+            )
+
+        # Check if boolean variable is either 0 or 1
+        if label_def.variant == LabelVariants.BOOLEAN in (0, 1):
+            raise HTTPException(
+                status_code=422,
+                detail=f"Value `{value}` of label `{label}` must be either 0 or 1.",
+            )
+
+    # Get and update sample
+    try:
+        db_sample = (
+            db_session.query(sample.Sample)
+            .filter_by(sample_id=sample_id, dataset_id=dataset_id)
+            .one()
+        )
+    except exc.NoResultFound as error:
+        raise HTTPException(
+            status_code=404, detail=f"No sample found with id `{sample_id}`"
+        ) from error
+
+    db_sample.labels = data.labels
+    db_session.commit()
+
+    return dict()
---
+++ backend/src/util/constants.py
@@ -0,0 +1,10 @@
+"""
+Application constants
+"""
+
+
+class LabelVariants:
+    """Valid values for the `variant` field of a label_definition"""
+
+    NUMERICAL = "numerical"
+    BOOLEAN = "boolean"
