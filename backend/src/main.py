9d74fe2eef7ff50119bc7b82c4bb549e74cd7d6c
Callum Gundlach <gundlachcallum@gmail.com>
2021-05-09 11:48:23 -0400

    OpenAPI metadata filled in

--- backend/src/main.py
+++ backend/src/main.py
@@ -15,7 +15,11 @@ if not os.path.exists(LABLR_DIR):
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Lablr Backend",
+    description="Backend for the Lablr annotation tool",
+    version="0.0.0",
+)
 
 app.include_router(datasets.router)
 app.include_router(samples.router)
